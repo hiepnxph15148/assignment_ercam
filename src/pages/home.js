e2134734df08147199adb08f33093a032c6020f2
hiepnxph <hiepnxph@gmail.com>
2022-01-10 20:05:57 +0700

    uplate

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,25 @@
 import Banner from "../components/banner";
 import NewsList from "../components/newList";
-
+import menu  from "../components/listMenu";
+import footer from "../components/footer";
 const HomePage = {
     print() {
         return /* html */`
+            <div class="menu">
+            ${menu.print()}
+            </div>
             <div class="banner">
                 ${Banner.print()}
             </div>
             <div class="news">
                 ${NewsList.print()}
             </div>
-            <div class="news">
+            <div class="news py-3">
                 ${NewsList.print()}
-            </div
+            </div>
+            </div class="footer">
+                ${footer.print()}
+            </div>
         `;
     },
 };
