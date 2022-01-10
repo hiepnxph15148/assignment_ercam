19eb859cf2e7a3dc942fa8e9635278f21595bd6d
hiepnxph <hiepnxph@gmail.com>
2022-01-10 19:47:35 +0700

    uplate

--- src/data.js
+++ src/data.js
@@ -17,24 +17,7 @@ const data = [
         img: "https://picsum.photos/400/400",
         desc: "Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018",
     },
-    {
-        id: 4,
-        title: "Tiêu đề bài viết 4",
-        img: "https://picsum.photos/400/400",
-        desc: "Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018",
-    },
-    {
-        id: 5,
-        title: "Tiêu đề bài viết 5",
-        img: "https://picsum.photos/400/400",
-        desc: "Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018",
-    },
-    {
-        id: 6,
-        title: "Tiêu đề bài viết 6",
-        img: "https://picsum.photos/400/400",
-        desc: "Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018 Vinh danh 295 học viên FPT Polytechnic học kì Spring 2018",
-    },
+    
 ];
 
 export default data;
